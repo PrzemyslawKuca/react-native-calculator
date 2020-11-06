547bc19291ac4c40717faa880d8d7348316bef50
Przemysław Kuca <evildemage@gmail.com>
2020-11-06 19:46:22 +0100

    Splash Screen

--- App.js
+++ App.js
@@ -1,61 +1,69 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Button from './components/Button';
-import calculator, {initialState} from './utilities/operations'
-import {ButtonsArray} from './assets/ButtosArray'
+import calculator, {initialState} from './utilities/operations';
+import {ButtonsArray} from './assets/ButtosArray';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class App extends React.Component {
-    constructor() {
-        super();
-        this.state = initialState;
-    }
+  constructor() {
+    super();
+    this.state = initialState;
+  }
 
-    handleTap = (type, value) => {
-        this.setState(state => calculator(type, value, state));
-    };
+  handleTap = (type, value) => {
+    this.setState((state) => calculator(type, value, state));
+  };
 
-    render() {
-        const {value} = this.state;
-        return (
-            <View style={styles.container}>
-                <Text style={styles.resultText}>{value}</Text>
-                <View style={styles.keyBoard}>
-                    {ButtonsArray.map((row, number) => (
-                            <View style={{flexDirection: "row"}} key={number}>
-                                {row.map((item, index) =>
-                                    <Button gray={item.gray} hidePortrait={item.hidePortrait}
-                                            double={item.double} orange={item.orange}
-                                            key={(number.toString() + index.toString())}
-                                            operation={() => {
-                                                this.handleTap(item.operation, item.value)
-                                            }}
-                                    >{item.name}</Button>
-                                )}
-                            </View>
-                        )
-                    )}
-                </View>
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    const {value} = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.resultText}>{value}</Text>
+        <View style={styles.keyBoard}>
+          {ButtonsArray.map((row, number) => (
+            <View style={{flexDirection: 'row'}} key={number}>
+              {row.map((item, index) => (
+                <Button
+                  gray={item.gray}
+                  hidePortrait={item.hidePortrait}
+                  double={item.double}
+                  orange={item.orange}
+                  key={number.toString() + index.toString()}
+                  operation={() => {
+                    this.handleTap(item.operation, item.value);
+                  }}>
+                  {item.name}
+                </Button>
+              ))}
             </View>
-        );
-    }
+          ))}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#545557",
-        display: 'flex',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
-        height: '100%',
-    },
-    resultText: {
-        color: '#FFF',
-        fontSize: 80,
-    },
-    keyBoard: {
-        backgroundColor: "#545557",
-        alignItems: 'flex-end',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-    },
+  container: {
+    backgroundColor: '#545557',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    height: '100%',
+  },
+  resultText: {
+    color: '#FFF',
+    fontSize: 80,
+  },
+  keyBoard: {
+    backgroundColor: '#545557',
+    alignItems: 'flex-end',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
 });
